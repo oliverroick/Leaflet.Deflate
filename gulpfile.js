1691e75e18513bdb62e16c106d8ac1a35b4e4cc6
Oliver Roick <oliver.roick@gmail.com>
2018-09-12 18:47:40 +0200

    Remove gulp

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-
-gulp.task('dist', function() {
-  gulp.src('src/L.Deflate.js')
-    .pipe(uglify().on('error', function(e){console.log(e);}))
-    .pipe(gulp.dest('dist'))
-});
